9eba9b9e77e923f0d55772ca61e114b71fff590d
vitaliymig <vitaliy.mooving@gmail.com>
2021-09-16 19:20:24 +0300

    background change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   
   return (
     <div className="container">
-      <div><img src="https://klike.net/uploads/posts/2019-06/1559370578_1.jpg" alt="mountains"/></div>
+      {/* <div><img src="https://klike.net/uploads/posts/2019-06/1559370578_1.jpg" alt="mountains"/></div> */}
       <div className="container-short"> 
       <h2 className="title-todo">T O D O</h2>
       <AddTodo></AddTodo> 
